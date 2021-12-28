a6adf349104d8212ce79cb071d1e4e2a873209e3
Nemanja <85313707+KKOO727@users.noreply.github.com>
2021-12-28 07:43:02 +0100

    fixed the batch mark bugs

--- resources/views/inventory/batch/editproduct.blade.php
+++ resources/views/inventory/batch/editproduct.blade.php
@@ -29,7 +29,8 @@
         <div class="page-inner">
             @if($containers)
                 @foreach($containers as $key => $value)
-                    <input type="checkbox" name="filter_container" class = "form-control" id="filter_container" value="{{ $value->owner_name }}" />{{ $value->owner_name }}
+                    <label>{{ $value->owner_name }}</label>
+                    <input type="checkbox" name="filter_container" class = "form-control" id="filter_container" value="{{ $value->owner_name }}" />
                 @endforeach
             @endif
         </div>
@@ -98,77 +99,84 @@
                                                                 <tbody>
                                                                     @foreach ($allproductdetail as $prod)
                                                                         @if ($prod->category_id == $cus->id)
-                                                                        @if($prod->initial_stock > 0)
-                                                                            <tr id="row_{{ $tbl_inc }}" class="getRow">
-                                                                                <td>
-                                                                                    <label>{{ $prod->product_name }}</label>
-                                                                                    <input type="hidden" name="prodName[]"
-                                                                                        value="{{ $prod->product_id }}" />
-                                                                                    <input type="hidden" name="cat_id[]"
-                                                                                        value="{{ $prod->category_id }}" />
-                                                                                </td>
-                                                                                <td>
-                                                                                    <label style="font-weight: bolder;">{{ $prod->initial_stock }}</label>
-                                                                                    <input type="hidden" name="initial_stock[]"
-                                                                                        class="iStock_{{ $tbl_inc }}"
-                                                                                        value="{{ $prod->initial_stock }}" />
-                                                                                </td>
-                                                                                <td>
-                                                                                    <input type="hidden" class="form-control"
-                                                                                        name="cost[]"
-                                                                                        value="{{ $prod->cost }}" />
-                                                                                    <input type="text" readonly
-                                                                                        class="form-control stock_{{ $tbl_inc }}"
-                                                                                        name="stock[]"
-                                                                                        value="{{ $prod->after_stock }}" style="font-weight: bolder;" /></td>
+                                                                            @if($prod->initial_stock > 0)
+                                                                                <tr id="row_{{ $tbl_inc }}" class="getRow">
+                                                                                    <td>
+                                                                                        <label>{{ $prod->product_name }}</label>
+                                                                                        <input type="hidden" name="prodName[]"
+                                                                                            value="{{ $prod->product_id }}" />
+                                                                                        <input type="hidden" name="cat_id[]"
+                                                                                            value="{{ $prod->category_id }}" />
+                                                                                    </td>
+                                                                                    <td>
+                                                                                        <label style="font-weight: bolder;">{{ $prod->initial_stock }}</label>
+                                                                                        <input type="hidden" name="initial_stock[]"
+                                                                                            class="iStock_{{ $tbl_inc }}"
+                                                                                            value="{{ $prod->initial_stock }}" />
+                                                                                    </td>
+                                                                                    <td>
+                                                                                        <input type="hidden" class="form-control"
+                                                                                            name="cost[]"
+                                                                                            value="{{ $prod->cost }}" />
+                                                                                        <input type="text" readonly
+                                                                                            class="form-control stock_{{ $tbl_inc }}"
+                                                                                            name="stock[]"
+                                                                                            value="{{ $prod->after_stock }}" style="font-weight: bolder;" /></td>
 
-                                                                                @if (isset($allmarkdetail[$tbl_inc - 1]))
-                                                                                    @php
-                                                                                        $markinc = 1;
-                                                                                        $incMarkData = $allmarkdetail[$tbl_inc - 1]['mark_data'];
-                                                                                        $incMarkData = json_decode($incMarkData);
-
-                                                                                    @endphp
-                                                                                    @foreach ($allmarks as $key => $mark)
+                                                                                    @if (isset($allmarkdetail[$tbl_inc - 1]))
                                                                                         @php
-                                                                                            $makrval = $incMarkData[$markinc-1];
+                                                                                            $markinc = 1;
+                                                                                            $incMarkData = $allmarkdetail[$tbl_inc - 1]['mark_data'];
+                                                                                            $incMarkData = json_decode($incMarkData);
                                                                                         @endphp
 
+                                                                                        @foreach ($allmarks as $key => $mark)
+                                                                                            @if(isset($incMarkData[$markinc-1]))
+                                                                                                @php
+                                                                                                    $makrval = $incMarkData[$markinc-1];
+                                                                                                @endphp
+
+                                                                                                <td>
+                                                                                                    <input type="text"
+                                                                                                        value="{{ $makrval }}"
+                                                                                                        name="mark_{{ ($key + 1) + count($allmarks) * ($prod->product_id - 1) }}"
+                                                                                                        class="form-control mkkk mark_{{ $tbl_inc }}" <?php if($batch->status == 1) { echo ""; } else { echo "readonly"; } ?> />
+                                                                                                </td>
+
+                                                                                                @php
+                                                                                                    $td_inc++;
+                                                                                                    $markinc++;
+                                                                                                @endphp
+                                                                                            @else
+                                                                                                <td>
+                                                                                                    <input type="text" value="0" name="mark_{{ ($key + 1) + count($allmarks) * ($prod->product_id - 1) }}" class="form-control mkkk mark_{{ $tbl_inc }}" />
+                                                                                                </td>
+                                                                                            @endif
+                                                                                        @endforeach
+                                                                                    @else
                                                                                         <td>
                                                                                             <input type="text"
-                                                                                                value="{{ $makrval }}"
-                                                                                                name="mark_{{ ($key + 1) + count($allmarks) * ($prod->product_id - 1) }}"
-                                                                                                class="form-control mkkk mark_{{ $tbl_inc }}" <?php if($batch->status == 1) { echo ""; } else { echo "readonly"; } ?> />
+                                                                                                name="mark_{{ $td_inc }}"
+                                                                                                class="form-control mkkk mark_{{ $tbl_inc }}" />
                                                                                         </td>
+                                                                                    @endif
 
-                                                                                        @php
-                                                                                            $td_inc++;
-                                                                                            $markinc++;
-                                                                                        @endphp
-                                                                                    @endforeach
-                                                                                @else
-                                                                                    <td>
-                                                                                        <input type="text"
-                                                                                            name="mark_{{ $td_inc }}"
-                                                                                            class="form-control mkkk mark_{{ $tbl_inc }}" />
-                                                                                    </td>
-                                                                                @endif
-                                                                                @if ($batch->status == 1)
-                                                                                    <td>
-                                                                                        <button type="button"
-                                                                                            onclick='deleteTblRow(this)'
-                                                                                            class="btn btn-danger btn-sm"><i
-                                                                                                class="fa fa-trash"></i></button>
-                                                                                    </td>
-                                                                                @else
-                                                                                    
-                                                                                @endif
-                                                                            </tr>
+                                                                                    @if ($batch->status == 1)
+                                                                                        <td>
+                                                                                            <button type="button"
+                                                                                                onclick='deleteTblRow(this)'
+                                                                                                class="btn btn-danger btn-sm"><i
+                                                                                                    class="fa fa-trash"></i></button>
+                                                                                        </td>
+                                                                                    @else
+                                                                                        
+                                                                                    @endif
+                                                                                </tr>
 
-                                                                            @php
-                                                                                $tbl_inc++;
-                                                                            @endphp
-                                                                        @endif
+                                                                                @php
+                                                                                    $tbl_inc++;
+                                                                                @endphp
+                                                                            @endif
                                                                         @endif
                                                                     @endforeach
                                                                 </tbody>
